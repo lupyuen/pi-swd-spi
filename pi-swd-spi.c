406e002edfdd6db0531a7d047957ab543611f815
lupyuen <luppy@appkaki.com>
2020-01-13 11:37:19 +0800

    Adding SWD command

--- pi-swd-spi.c
+++ pi-swd-spi.c
@@ -73,27 +73,20 @@ static const unsigned swd_seq_jtag_to_swd_len = 136;
 
 //  End of https://github.com/ntfreak/openocd/blob/master/src/jtag/swd.h
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-
-static void pabort(const char *s)
-{
-	perror(s);
-	abort();
-}
-
 static const char *device = "/dev/spidev1.1";
 static uint8_t mode = 0  //  Note: LSB mode is not supported on Broadcom. We must flip LSB to MSB ourselves.
-    //| SPI_CPOL   //  Clock polarity
-    //| SPI_CPHA   //  Clock phase
     | SPI_NO_CS  //  1 device per bus, no Chip Select
     | SPI_3WIRE  //  Bidirectional mode, data in and out pin shared
-    ;
-static uint8_t bits = 8;
-static uint32_t speed = 122000;  //  Previously 500000
-static uint16_t delay = 0;
+    ;            //  Data is valid on first rising edge of the clock, so CPOL=0 and CPHA=0
+static uint8_t bits = 8;         //  8 bits per word
+static uint32_t speed = 122000;  //  122 kHz. Previously 500000
+static uint16_t delay = 0;       //  SPI driver latency: https://www.raspberrypi.org/forums/viewtopic.php?f=44&t=19489
 
-static void transfer(int fd)
-{
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+static void pabort(const char *s);
+
+static void transfer(int fd) {
 	int ret;
 	uint8_t tx[] = {
 		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
@@ -131,93 +124,7 @@ static void transfer(int fd)
 	puts("");
 }
 
-static void print_usage(const char *prog)
-{
-	printf("Usage: %s [-DsbdlHOLC3]\n", prog);
-	puts("  -D --device   device to use (default /dev/spidev1.1)\n"
-	     "  -s --speed    max speed (Hz)\n"
-	     "  -d --delay    delay (usec)\n"
-	     "  -b --bpw      bits per word \n"
-	     "  -l --loop     loopback\n"
-	     "  -H --cpha     clock phase\n"
-	     "  -O --cpol     clock polarity\n"
-	     "  -L --lsb      least significant bit first\n"
-	     "  -C --cs-high  chip select active high\n"
-	     "  -3 --3wire    SI/SO signals shared\n");
-	exit(1);
-}
-
-static void parse_opts(int argc, char *argv[])
-{
-	while (1) {
-		static const struct option lopts[] = {
-			{ "device",  1, 0, 'D' },
-			{ "speed",   1, 0, 's' },
-			{ "delay",   1, 0, 'd' },
-			{ "bpw",     1, 0, 'b' },
-			{ "loop",    0, 0, 'l' },
-			{ "cpha",    0, 0, 'H' },
-			{ "cpol",    0, 0, 'O' },
-			{ "lsb",     0, 0, 'L' },
-			{ "cs-high", 0, 0, 'C' },
-			{ "3wire",   0, 0, '3' },
-			{ "no-cs",   0, 0, 'N' },
-			{ "ready",   0, 0, 'R' },
-			{ NULL, 0, 0, 0 },
-		};
-		int c;
-
-		c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NR", lopts, NULL);
-
-		if (c == -1)
-			break;
-
-		switch (c) {
-		case 'D':
-			device = optarg;
-			break;
-		case 's':
-			speed = atoi(optarg);
-			break;
-		case 'd':
-			delay = atoi(optarg);
-			break;
-		case 'b':
-			bits = atoi(optarg);
-			break;
-		case 'l':
-			mode |= SPI_LOOP;
-			break;
-		case 'H':
-			mode |= SPI_CPHA;
-			break;
-		case 'O':
-			mode |= SPI_CPOL;
-			break;
-		case 'L':
-			mode |= SPI_LSB_FIRST;
-			break;
-		case 'C':
-			mode |= SPI_CS_HIGH;
-			break;
-		case '3':
-			mode |= SPI_3WIRE;
-			break;
-		case 'N':
-			mode |= SPI_NO_CS;
-			break;
-		case 'R':
-			mode |= SPI_READY;
-			break;
-		default:
-			print_usage(argv[0]);
-			break;
-		}
-	}
-}
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int ret = 0;
 	int fd;
 
@@ -270,3 +177,93 @@ int main(int argc, char *argv[])
 
 	return ret;
 }
+
+static void pabort(const char *s) {
+	perror(s);
+	abort();
+}
+
+#ifdef NOTUSED
+    static void print_usage(const char *prog) {
+        printf("Usage: %s [-DsbdlHOLC3]\n", prog);
+        puts("  -D --device   device to use (default /dev/spidev1.1)\n"
+            "  -s --speed    max speed (Hz)\n"
+            "  -d --delay    delay (usec)\n"
+            "  -b --bpw      bits per word \n"
+            "  -l --loop     loopback\n"
+            "  -H --cpha     clock phase\n"
+            "  -O --cpol     clock polarity\n"
+            "  -L --lsb      least significant bit first\n"
+            "  -C --cs-high  chip select active high\n"
+            "  -3 --3wire    SI/SO signals shared\n");
+        exit(1);
+    }
+
+    static void parse_opts(int argc, char *argv[]) {
+        while (1) {
+            static const struct option lopts[] = {
+                { "device",  1, 0, 'D' },
+                { "speed",   1, 0, 's' },
+                { "delay",   1, 0, 'd' },
+                { "bpw",     1, 0, 'b' },
+                { "loop",    0, 0, 'l' },
+                { "cpha",    0, 0, 'H' },
+                { "cpol",    0, 0, 'O' },
+                { "lsb",     0, 0, 'L' },
+                { "cs-high", 0, 0, 'C' },
+                { "3wire",   0, 0, '3' },
+                { "no-cs",   0, 0, 'N' },
+                { "ready",   0, 0, 'R' },
+                { NULL, 0, 0, 0 },
+            };
+            int c;
+
+            c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NR", lopts, NULL);
+
+            if (c == -1)
+                break;
+
+            switch (c) {
+            case 'D':
+                device = optarg;
+                break;
+            case 's':
+                speed = atoi(optarg);
+                break;
+            case 'd':
+                delay = atoi(optarg);
+                break;
+            case 'b':
+                bits = atoi(optarg);
+                break;
+            case 'l':
+                mode |= SPI_LOOP;
+                break;
+            case 'H':
+                mode |= SPI_CPHA;
+                break;
+            case 'O':
+                mode |= SPI_CPOL;
+                break;
+            case 'L':
+                mode |= SPI_LSB_FIRST;
+                break;
+            case 'C':
+                mode |= SPI_CS_HIGH;
+                break;
+            case '3':
+                mode |= SPI_3WIRE;
+                break;
+            case 'N':
+                mode |= SPI_NO_CS;
+                break;
+            case 'R':
+                mode |= SPI_READY;
+                break;
+            default:
+                print_usage(argv[0]);
+                break;
+            }
+        }
+    }
+#endif  // NOTUSED
