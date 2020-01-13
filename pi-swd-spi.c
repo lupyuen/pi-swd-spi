3e233f7953f8fa90d113b4471d14379c2682fdfa
lupyuen <luppy@appkaki.com>
2020-01-13 15:24:40 +0800

    SPI transmit and receive

--- pi-swd-spi.c
+++ pi-swd-spi.c
@@ -59,8 +59,8 @@ static const unsigned swd_seq_jtag_to_swd_len = 136;
 //  End of https://github.com/ntfreak/openocd/blob/master/src/jtag/swd.h
 
 //  Read register 0 (IDCODE)
-static const uint8_t read_reg_0[] = { 0xa5 };
-static const unsigned read_reg_0_len = 1;
+static const uint8_t swd_read_reg_0[] = { 0xa5 };
+static const unsigned swd_read_reg_0_len = 1;
 
 static const char *device = "/dev/spidev1.1";
 static uint8_t mode = 0  //  Note: LSB mode is not supported on Broadcom. We must flip LSB to MSB ourselves.
@@ -75,95 +75,143 @@ static uint16_t delay = 0;       //  SPI driver latency: https://www.raspberrypi
 
 static void pabort(const char *s);
 
-static void transfer(int fd) {
-	int ret;
-	uint8_t tx[] = {
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0x40, 0x00, 0x00, 0x00, 0x00, 0x95,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xDE, 0xAD, 0xBE, 0xEF, 0xBA, 0xAD,
-		0xF0, 0x0D,
-	};
-	uint8_t rx[ARRAY_SIZE(tx)] = {0, };
-    /* Bidirectional or "3-wire" mode is supported by the spi-bcm2835 kernel module. 
-       Please note that in this mode, either the tx or rx field of the spi_transfer 
-       struct must be a NULL pointer, since only half-duplex communication is possible. 
-       Otherwise, the transfer will fail. The spidev_test.c source code does not consider 
-       this correctly, and therefore does not work at all in 3-wire mode. */
+/// The byte at index i is the value of i with all bits flipped. https://stackoverflow.com/questions/746171/efficient-algorithm-for-bit-reversal-from-msb-lsb-to-lsb-msb-in-c
+static const uint8_t reverse_byte[] = {  
+  0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 
+  0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 
+  0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 
+  0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 
+  0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 
+  0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+  0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6, 
+  0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
+  0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+  0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9, 
+  0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
+  0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+  0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3, 
+  0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
+  0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7, 
+  0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
+};
+
+#define MAX_SPI_SIZE 256
+static uint8_t reverse_buf[MAX_SPI_SIZE];
+
+/* From https://www.raspberrypi.org/documentation/hardware/raspberrypi/spi/README.md:
+Bidirectional or "3-wire" mode is supported by the spi-bcm2835 kernel module. 
+Please note that in this mode, either the tx or rx field of the spi_transfer 
+struct must be a NULL pointer, since only half-duplex communication is possible. 
+Otherwise, the transfer will fail. The spidev_test.c source code does not consider 
+this correctly, and therefore does not work at all in 3-wire mode. */
+
+/// Transmit len bytes of buf (assumed to be in LSB format) to the SPI device in MSB format
+static void spi_transmit(int fd, uint8_t *buf, unsigned int len) {
+    {
+        printf("spi_transmit: len=%d\n", len);
+        for (unsigned int i = 0; i < len; i++) {
+            if (!(i % 6)) { puts(""); }
+            printf("%.2X ", reverse_buf[i]);
+        }
+        puts("");
+    }
+    //  Reverse LSB to MSB for entire buf into reversed buffer.
+    if (len >= MAX_SPI_SIZE) { printf("len=%d ", len); pabort("spi_transmit overflow"); return; }
+    for (unsigned int i = 0; i < len; i++) {
+        uint8_t b = buf[i];
+        reverse_buf[i] = reverse_byte[(uint8_t) b];
+    }
+    //  Transmit the reversed buffer to SPI device.
 	struct spi_ioc_transfer tr = {
-		.tx_buf = (unsigned long)tx,
-		.rx_buf = (unsigned long)rx,
-		.len = ARRAY_SIZE(tx),
+		.tx_buf = (unsigned long) reverse_buf,
+		.rx_buf = NULL,
+		.len = len,
 		.delay_usecs = delay,
 		.speed_hz = speed,
 		.bits_per_word = bits,
 	};
+	int ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+    //  Check SPI result.
+	if (ret < 1) { pabort("spi_transmit failed"); }
+}
 
-	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-	if (ret < 1)
-		pabort("can't send spi message");
+/// Receive len bytes from SPI device (assumed to be in MSB format) and write into buf in LSB format
+static void spi_receive(int fd, uint8_t *buf, unsigned int len) {
+    //  Receive the reversed buffer from SPI device.
+    printf("spi_receive: len=%d\n", len);
+    if (len >= MAX_SPI_SIZE) { printf("len=%d ", len); pabort("spi_receive overflow"); return; }
+	struct spi_ioc_transfer tr = {
+		.tx_buf = NULL,
+		.rx_buf = (unsigned long) reverse_buf,
+		.len = len,
+		.delay_usecs = delay,
+		.speed_hz = speed,
+		.bits_per_word = bits,
+	};
+	int ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+    //  Check SPI result.
+	if (ret < 1) { pabort("spi_receive failed"); }
+    //  Reverse MSB to LSB from reversed buffer into buf.
+    for (unsigned int i = 0; i < len; i++) {
+        uint8_t b = reverse_buf[i];
+        buf[i] = reverse_byte[(uint8_t) b];
+    }
+    {
+        for (unsigned int i = 0; i < len; i++) {
+            if (!(i % 6)) { puts(""); }
+            printf("%.2X ", buf[i]);
+        }
+        puts("");
+    }
+}
 
-	for (ret = 0; ret < ARRAY_SIZE(tx); ret++) {
-		if (!(ret % 6))
-			puts("");
-		printf("%.2X ", rx[ret]);
-	}
-	puts("");
+/// Transmit and receive dat to SPI device
+static void spi_transfer(int fd) {
+    //  Transmit JTAG-to-SWD sequence.
+    spi_transmit(fd, swd_seq_jtag_to_swd, swd_seq_jtag_to_swd_len);
+
+    //  Transmit command to read register 0 (IDCODE).
+    spi_transmit(fd, swd_read_reg_0, swd_read_reg_0_len);
+
+    //  Read response (38 bits)
+    const int buf_size = 5;
+    uint8_t buf[5]
+    spi_receive(fd, buf, buf_size);
 }
 
 int main(int argc, char *argv[]) {
-	int ret = 0;
-	int fd;
-
-	parse_opts(argc, argv);
-
-	fd = open(device, O_RDWR);
-	if (fd < 0)
-		pabort("can't open device");
-
-	/*
-	 * spi mode
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
-	if (ret == -1)
-		pabort("can't set spi mode");
-
-	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
-	if (ret == -1)
-		pabort("can't get spi mode");
-
-	/*
-	 * bits per word
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-	if (ret == -1)
-		pabort("can't set bits per word");
-
-	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
-	if (ret == -1)
-		pabort("can't get bits per word");
-
-	/*
-	 * max speed hz
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-	if (ret == -1)
-		pabort("can't set max speed hz");
-
-	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-	if (ret == -1)
-		pabort("can't get max speed hz");
-
+	//  Previously: parse_opts(argc, argv);
 	printf("spi mode: %d\n", mode);
 	printf("bits per word: %d\n", bits);
 	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
 
-	transfer(fd);
+    //  Open SPI device.
+	int fd = open(device, O_RDWR);
+	if (fd < 0) { pabort("can't open device"); }
 
+    //  Set SPI mode to read and write.
+	int ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+	if (ret == -1) { pabort("can't set spi mode"); }
+	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+	if (ret == -1) { pabort("can't get spi mode"); }
+
+    //  Set SPI read and write bits per word.
+	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret == -1) { pabort("can't set bits per word"); }
+	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret == -1) { pabort("can't get bits per word"); }
+
+    //  Set SPI read and write max speed.
+	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret == -1) { pabort("can't set max speed hz"); }
+	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret == -1) { pabort("can't get max speed hz"); }
+
+    //  Transmit and receive to/from SPI device.
+	spi_transfer(fd);
+
+    //  Close SPI device.
 	close(fd);
-
 	return ret;
 }
 
@@ -173,6 +221,44 @@ static void pabort(const char *s) {
 }
 
 #ifdef NOTUSED
+    static void transfer(int fd) {
+        int ret;
+        uint8_t tx[] = {
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0x40, 0x00, 0x00, 0x00, 0x00, 0x95,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xDE, 0xAD, 0xBE, 0xEF, 0xBA, 0xAD,
+            0xF0, 0x0D,
+        };
+        uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+        /* Bidirectional or "3-wire" mode is supported by the spi-bcm2835 kernel module. 
+        Please note that in this mode, either the tx or rx field of the spi_transfer 
+        struct must be a NULL pointer, since only half-duplex communication is possible. 
+        Otherwise, the transfer will fail. The spidev_test.c source code does not consider 
+        this correctly, and therefore does not work at all in 3-wire mode. */
+        struct spi_ioc_transfer tr = {
+            .tx_buf = (unsigned long)tx,
+            .rx_buf = (unsigned long)rx,
+            .len = ARRAY_SIZE(tx),
+            .delay_usecs = delay,
+            .speed_hz = speed,
+            .bits_per_word = bits,
+        };
+
+        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+        if (ret < 1)
+            pabort("can't send spi message");
+
+        for (ret = 0; ret < ARRAY_SIZE(tx); ret++) {
+            if (!(ret % 6))
+                puts("");
+            printf("%.2X ", rx[ret]);
+        }
+        puts("");
+    }
+
     static void print_usage(const char *prog) {
         printf("Usage: %s [-DsbdlHOLC3]\n", prog);
         puts("  -D --device   device to use (default /dev/spidev1.1)\n"
